a7b338e599a58e956f96d24bd2cb2149da96d2c4
jacek.marchwicki@gmail.com <jacek.marchwicki@gmail.com@a345089e-d5f6-8901-d31c-43efdc9a10fd>
2011-03-11 02:03:18 +0000

    More cleaning

--- com.downloader/src/com/downloader/Services/DownloadServiceThread.java
+++ com.downloader/src/com/downloader/Services/DownloadServiceThread.java
@@ -1,9 +1,5 @@
 package com.downloader.Services;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
-
 import android.content.Context;
 import android.database.ContentObserver;
 import android.database.Cursor;
@@ -32,82 +28,32 @@ public class DownloadServiceThread extends Thread {
 		}
 	}
 
-
-	private DownloadServiceThreadData data = new DownloadServiceThreadData(
-			new HashMap<Long, DownloadItem>());
 	private Notifications notifications;
 
 	public DownloadServiceThread() {
 		super("Hotfile downloader service");
 	}
 
-	private void deleteDownload(long id) {
-		DownloadItem downloadItem = data.downloads.get(id);
-		if (downloadItem.getStatus() == Variables.STATUS_RUNNING)
-			downloadItem.setStatus(Variables.STATUS_CANCEL);
-		data.downloads.remove(id);
-	}
-
-	private DownloadItem insertDownload(DownloadItem.Warehouse warehouse,
-			long now) {
-		DownloadItem downloadItem = warehouse.addNewItem(context);
-		data.downloads.put(downloadItem.getId(), downloadItem);
-		Log.v(LOG_TAG, "Downloading file: " + downloadItem.getRequestUri());
-		DownloadItemWorker downloadItemWorker = new DownloadItemWorker(downloadItem);
-		downloadItemWorker.tryToStartDownload();
-		return downloadItem;
-
-	}
-
-	private void updateDownload(DownloadItem.Warehouse warehouse,
-			DownloadItem downloadItem) {
-		warehouse.updateItemFromDatabase(downloadItem);
-		DownloadItemWorker downloadItemWorker = new DownloadItemWorker(downloadItem);
-		downloadItemWorker.tryToStartDownload();
-	}
-
 	private synchronized void contentProviderChanged(){
-		long now = System.currentTimeMillis();
 		notifications.updateNotification();
-		Set<Long> idsNoLongerInDB =
-			new HashSet<Long>(data.downloads.keySet());
 
 		Cursor cursor = context.getContentResolver().query(
 				Variables.CONTENT_URI, null, null, null, null);
 		try {
 			DownloadItem.Warehouse reader = new DownloadItem.Warehouse(
 					cursor);
-			int idColumn = cursor
-			.getColumnIndexOrThrow(Variables.DB_COLUMN_ID);
 			if (cursor.getCount() > 0) {
 				cursor.moveToFirst();
 				do {
-					long id = cursor.getLong(idColumn);
-					idsNoLongerInDB.remove(id);
-					DownloadItem downloadItem = data.downloads.get(id);
-					if (downloadItem != null)
-						updateDownload(reader, downloadItem);
-					else
-						downloadItem = insertDownload(reader, now);
-					// TODO nie wiem co skopiowac z download service
-					// line 246
+					DownloadItem downloadItem = reader.addNewItem(context);
+					Log.v(LOG_TAG, "Downloading file: " + downloadItem.getRequestUri());
+					DownloadItemWorker downloadItemWorker = new DownloadItemWorker(downloadItem);
+					downloadItemWorker.tryToStartDownload();
 
 				} while (cursor.moveToNext());
 			}
 		} finally {
 			cursor.close();
-		}
-
-		for (Long id : idsNoLongerInDB)
-			deleteDownload(id);
-		for (DownloadItem downloadItem : data.downloads.values()) {
-			if (downloadItem.deleted)
-				context.getContentResolver()
-				.delete(
-						Variables.CONTENT_URI,
-						Variables.DB_COLUMN_ID + " = ? ",
-						new String[] { String
-								.valueOf(downloadItem.getId()) });
 		}
 	}
 
